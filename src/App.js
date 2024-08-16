1fb9d14c890b541d0ad1a7e19ea022e08b13ab0f
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-08-17 00:03:36 +0200

    feat: dynamic naming

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
@@ -6,8 +6,13 @@ import Experience from './components/Experience';
 import Education from './components/Education';
 import Publications from './components/Publications';
 import Projects from './components/Projects';
+import aboutData from './data/about.json';
 
 function App() {
+  useEffect(() => {
+    document.title = `${aboutData.name}`;
+  }, []);
+
   return (
     <div className="App">
       <Header />
